94b6479f6ef1af5f8c84c051b7dee62924e700b4
Maciej Ziaja <maciejzjg@gmail.com>
2020-04-27 19:26:16 +0200

    Convert input from deg to rad

--- src/ahrs_text_buffer.cpp
+++ src/ahrs_text_buffer.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <sstream>
 #include <vector>
 
@@ -6,6 +7,8 @@
 #include "ahrs_viewer/sensor_from_text.h"
 
 namespace txtsens {
+
+double to_rad(double deg) { return deg * M_PI / 180; }
 
 std::vector<std::string> split_str(const std::string& str, const char delim) {
     std::vector<std::string> ret;
@@ -20,16 +23,28 @@ std::vector<std::string> split_str(const std::string& str, const char delim) {
 }
 
 void AhrsTextBuffer::parse_readout(std::string readout) {
-    auto splits = split_str(readout, ',');
+    enum csv_pos : size_t {
+        dt_p = 0, accx_p, accy_p, accz_p,
+        gyrox_p, gyroy_p, gyroz_p,
+        magx_p, magy_p, magz_p
+    };
 
+    auto splits = split_str(readout, ',');
     std::vector<double> numerized(splits.size());
+
     std::transform(splits.begin(), splits.end(), numerized.begin(),
                    [](const std::string& s) { return std::stod(s); });
+    std::transform(numerized.begin() + gyrox_p, numerized.begin() + gyroz_p + 1,
+                   numerized.begin() + gyrox_p,
+                   [](const double d) { return to_rad(d); });
 
-    dt = numerized[0];
-    gyror = ahrs::sensor_readout{numerized[1], numerized[2], numerized[3]};
-    accr = ahrs::sensor_readout{numerized[4], numerized[5], numerized[6]};
-    magr = ahrs::sensor_readout{numerized[7], numerized[8], numerized[9]};
+    dt = numerized[dt_p];
+    accr = ahrs::sensor_readout{numerized[accx_p], numerized[accy_p],
+                                numerized[accz_p]};
+    gyror = ahrs::sensor_readout{numerized[gyrox_p], numerized[gyroy_p],
+                                 numerized[gyroz_p]};
+    magr = ahrs::sensor_readout{numerized[magx_p], numerized[magy_p],
+                                numerized[magz_p]};
 }
 
 bool AhrsTextBuffer::fetch() {
